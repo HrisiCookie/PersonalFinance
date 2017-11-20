9bb6aec68c74efd6463c24abd6c480f3eb394eda
Hristina Bailova <cookie@Hristinas-MacBook-Air.local>
2017-11-21 00:48:50 +0200

    TextField extension

---
+++ PersonalFinance/Extensions/UITextFieldExtension.swift
@@ -0,0 +1,17 @@
+//
+//  UITextFieldExtension.swift
+//  PersonalFinance
+//
+//  Created by Hristina Bailova on 11/20/17.
+//  Copyright © 2017 Hristina Bailova. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UITextField {
+    func transparency() {
+        self.backgroundColor = UIColor.white
+        self.alpha = 0.8
+    }
+}
