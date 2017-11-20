32d7269a5e8aab891203cce59a3d986c6d1a97c8
Hristina Bailova <cookie@Hristinas-MacBook-Air.local>
2017-11-20 23:36:23 +0200

    Added API urls for login and registration

---
+++ PersonalFinance/Utils/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  PersonalFinance
+//
+//  Created by Hristina Bailova on 11/20/17.
+//  Copyright © 2017 Hristina Bailova. All rights reserved.
+//
+
+import Foundation
+
+struct APIURLs {
+    static let baseUrl = "http://cookie-rocks.com"
+    static let loginUrl = "\(baseUrl)/accounts/rest-auth/login/"
+}
